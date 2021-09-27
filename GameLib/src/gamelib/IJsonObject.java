54d2d8beb14391a7a2c25c3f33c71fbdf6f3b5ee
Yosua Blanco <yosuabd@estudiantec.cr>
2021-09-27 17:45:42 -0600

    Libreria para leer json.
    Para yo compilar uso javac.source=1.8. Por si les da problemas pasenlo a 9.

---
+++ GameLib/src/gamelib/IJsonObject.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamelib;
+
+/**
+ *
+ * @author Yosua Blanco Diaz
+ */
+public interface IJsonObject {
+     public void SaveJson();
+     public void LoadJson(String nameFile);
+}
